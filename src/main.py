33501d54fc724d67da0820452d3e44c33cc406b9
Quoc Mien <quocmienn@gmail.com>
2023-03-30 00:54:00 +0700

    update

--- src/main.py
+++ src/main.py
@@ -1,26 +1,53 @@
-# call api openai from chatbot messenger
-
-
-import openai
 from dotenv import load_dotenv
 import os
+from facebook_webhook import (
+    FlaskServer,
+    FacebookWebhook,
+    make_quick_reply,
+    make_text_message,
+    make_payload,
+)
+import openai
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
-load_dotenv(dotenv_path)
+# Load các biến môi trường từ file .env
+load_dotenv()
 
+# Lấy API key của OpenAI từ biến môi trường
 api_key = os.getenv("OPENAI_API_KEY")
 
+# Khởi tạo OpenAI API client
+openai.api_key = api_key
 
-# Define your prompt for the OpenAI API
-prompt = input("Enter your prompt: ")
+# Khởi tạo Facebook Webhook
+webhook = FacebookWebhook()
+app = FlaskServer(webhook)
 
-# Call the OpenAI API to generate a response
-response = openai.Completion.create(
-  engine='text-davinci-003', 
-  prompt=prompt, 
-  max_tokens=100
-)
+# Định nghĩa một hàm để xử lý tin nhắn và trả lời bằng OpenAI API
+def handle_message(sender_id, text):
+    # Gửi câu hỏi đến OpenAI API và nhận lại câu trả lời
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=f"Q: {text}\nA:",
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    answer = response.choices[0].text.strip()
+    
+    # Trả lời tin nhắn với câu trả lời của OpenAI
+    quick_replies = [
+        make_quick_reply("Yes", make_payload("yes")),
+        make_quick_reply("No", make_payload("no")),
+    ]
+    message = make_text_message(answer, quick_replies=quick_replies)
+    webhook.send(sender_id, message)
 
-# Retrieve the generated response and print it
-message = response.choices[0].text.strip()
-print(message)
+# Đăng ký một handler cho tin nhắn
+@webhook.default_message
+def handle_default(sender_id, message):
+    handle_message(sender_id, message.text)
+
+# Chạy ứng dụng Flask
+if __name__ == "__main__":
+    app.run()

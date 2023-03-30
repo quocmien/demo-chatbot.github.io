ab0017e0cb239729b1ee5fc44f1aa1aeb682e398
Quoc Mien <quocmienn@gmail.com>
2023-03-30 11:55:50 +0700

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -1,30 +1,18 @@
-require('dotenv').config();
-
-import request from 'request';
-const fetch = require('node-fetch');
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+require("dotenv").config;
+import request from "request";
+import axios from "axios";
 
 let getHomePage = (req, res) => {
-  return res.send('Xin Chao');
+  return res.send("Xin Chào");
 };
-
-// Set up your OpenAI API key
-const openai = require('openai');
-openai.api_key = process.env.OPENAI_API_KEY;
-
-// Define your prompt for the OpenAI API
-const prompt = 'Enter your prompt: ';
-
-// Run the Python script and get the result
-
 
 let postWebhook = (req, res) => {
   // Parse the request body from the POST
   let body = req.body;
+  console.log('request hook', req)
 
   // Check the webhook event is from a Page subscription
-  if (body.object === 'page') {
+  if (body.object === "page") {
     // Iterate over each entry - there may be multiple if batched
     body.entry.forEach(function (entry) {
       // Gets the body of the webhook event
@@ -33,7 +21,7 @@ let postWebhook = (req, res) => {
 
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
-      console.log('Sender PSID: ' + sender_psid);
+      console.log("Sender PSID: " + sender_psid);
 
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
@@ -45,73 +33,88 @@ let postWebhook = (req, res) => {
     });
 
     // Return a '200 OK' response to all events
-    res.status(200).send('EVENT_RECEIVED');
+    res.status(200).send("EVENT_RECEIVED");
   } else {
     // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
 };
-
 let getWebhook = (req, res) => {
   // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
 
   // Check if a token and mode is in the query string of the request
   if (mode && token) {
     // Check the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
       // Respond with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send
-
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Respond with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
     }
   }
 };
 // Handles messages events
 async function handleMessage(sender_psid, received_message) {
+  let response;
+
   // Checks if the message contains text
   if (received_message.text) {
-    // Get the user's message
-    const userMessage = received_message.text;
-
-    // Define your prompt for the OpenAI API with the user's message
-    const prompt = `User: ${userMessage}\n${' '.repeat('User: '.length)}`;
-
-    // Send a request to the OpenAI API
-    const result = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
+    await axios.post('https://modelgpt.tessolutions.net/chat', {
+      text: received_message.text
+    }).then((res) => {
+      console.log('res', res.data)
+      response = {
+        text: res.data.data
+      }
+    })
+    .catch((err) => {
+      console.log('err', err)
+    })
+    // response = {
+    //   text: `You sent the message: "${received_message.text}". Now send me an attachment!`,
+    // };
+  } else if (received_message.attachments) {
+    // Get the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [
+            {
+              title: "Is this the right picture?",
+              subtitle: "Tap a button to answer.",
+              image_url: attachment_url,
+              buttons: [
+                {
+                  type: "postback",
+                  title: "Yes!",
+                  payload: "yes",
+                },
+                {
+                  type: "postback",
+                  title: "No!",
+                  payload: "no",
+                },
+              ],
+            },
+          ],
+        },
       },
-      body: JSON.stringify({
-        prompt: prompt,
-        max_tokens: 100,
-        temperature: 0.7,
-        n: 1,
-        stop: '\n'
-      })
-    });
-
-    // Get the response from the OpenAI API
-    const data = await result.json();
-    const message = data.choices[0].text.trim();
-
-    console.log(message); // Print the generated response
-
-    // Create the payload for the response message
-    const response = {
-      text: message,
     };
+  }
 
-    // Send the response message
-    callSendAPI(sender_psid, response);
-  } 
+  // Send the response message
+  callSendAPI(sender_psid, response);
 }
-
-
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
@@ -143,8 +146,8 @@ function callSendAPI(sender_psid, response) {
   // Send the HTTP request to the Messenger Platform
   request(
     {
-      uri: "https://graph.facebook.com/v12.0/me/messages",
-      qs: { access_token: PAGE_ACCESS_TOKEN },
+      uri: "https://graph.facebook.com/v2.6/me/messages",
+      qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
       method: "POST",
       json: request_body,
     },
